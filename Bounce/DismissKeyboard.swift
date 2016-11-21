08cec904aa47bb6e71b85348199cbbd69e52cf04
jitaekim93@gmail.com <jitaekim93@gmail.com>
2016-11-21 15:27:32 -0800

    added slide in animations to login page

---
+++ Bounce/DismissKeyboard.swift
@@ -0,0 +1,9 @@
+//
+//  DismissKeyboard.swift
+//  Bounce
+//
+//  Created by Jitae Kim on 11/21/16.
+//  Copyright © 2016 Jitae Kim. All rights reserved.
+//
+
+import Foundation
---
+++ Bounce/RoundButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundButton.swift
+//  Bounce
+//
+//  Created by Jitae Kim on 11/21/16.
+//  Copyright © 2016 Jitae Kim. All rights reserved.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
